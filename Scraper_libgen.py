83e90f86e1b2600647ce85dc2cb4b76d90f62e3e
Reti <r.w@hotmail.ch>
2021-07-04 18:33:24 +0200

    I get errors, when scraping proxy sites, what bout you? Ich glaub au, dass mini function und de for-loop ned suber lauft, aber, da de error scho vorher cho isch, hanni das ned usprobiert ^^

--- Scraper_libgen.py
+++ Scraper_libgen.py
@@ -20,16 +20,19 @@ for prox in proxy_type:
 
 #get Proxies
 with open('proxies.csv') as f:
-    list_proxy = list(csv.reader(f))
+    list_proxy = csv.reader(f)
+    proxy_list.append(list_proxy)
 
+print(proxy_list)
 
 """
-
-
 proxy_cycle = cycle(list_proxy)
 
 # Prime the pump
 proxy = next(proxy_cycle)
+
+suchbegriff = str(input("Suchbegriff:"))
+url_sb = suchbegriff.replace(" ", "+")
 
 for i in range(1, 10):          #würd (1, len(list_proxy)) ned meh Sinn mache?
     proxy = next(proxy_cycle)
@@ -37,87 +40,86 @@ for i in range(1, 10):          #würd (1, len(list_proxy)) ned meh Sinn mache?
       "http": proxy,
       "https":proxy
     }
+    try:
+        #start book scraper
+        debug = False
+        page_index = 1
+        all_book_links = []
+        while True:
+            URL = 'https://libgen.is/search.php?req=' + url_sb + f'&open=0&res=100&view=detailed&phrase=1&column=def&page={page_index}'
+            page = requests.get(URL, proxies=proxies)
+            soup = bs(page.content, 'html.parser')
 
-    #start book scraper
-    suchbegriff = str(input("Suchbegriff:"))
-    url_sb = suchbegriff.replace(" ", "+")
-    debug = False
-    page_index = 1
-    all_book_links = []
-    while True:
-        URL = 'https://libgen.is/search.php?req=' + url_sb + f'&open=0&res=100&view=detailed&phrase=1&column=def&page={page_index}'
-        page = requests.get(URL)
-        soup = bs(page.content, 'html.parser')
+            #getting the links to every book on a page
+            links = []
+            for i in soup.find_all('td', {'colspan' : '2'}):
+                link = i.find('a', href=True)
+                href_link = (link['href'])
+                str_link = str(href_link)
+                links.append(str_link.replace('..', ''))
+                if link is None:
+                    continue
 
-        #getting the links to every book on a page
-        links = []
-        for i in soup.find_all('td', {'colspan' : '2'}):
-            link = i.find('a', href=True)
-            href_link = (link['href'])
-            str_link = str(href_link)
-            links.append(str_link.replace('..', ''))
-            if link is None:
-                continue
+            #slug to books
+            new_book_links = []
+            regex = re.compile('.*book\/index\.php\?.*')
+            links_books = [n for n in links if regex.match(n)]
 
-        #slug to books
-        new_book_links = []
-        regex = re.compile('.*book\/index\.php\?.*')
-        links_books = [n for n in links if regex.match(n)]
-
-        #building the correct links directly to the book 
-        for e in links_books:
-            link_to_book = 'https://libgen.is' + str(e)
-            new_book_links.append(link_to_book)
-        if len(new_book_links) > 0:
-            all_book_links.extend(new_book_links)
-            page_index += 1
-            if debug:
+            #building the correct links directly to the book 
+            for e in links_books:
+                link_to_book = 'https://libgen.is' + str(e)
+                new_book_links.append(link_to_book)
+            if len(new_book_links) > 0:
+                all_book_links.extend(new_book_links)
+                page_index += 1
+                if debug:
+                    break
+            else:
                 break
-        else:
-            break
 
-    download_links = []
-    #open book links
-    for link in all_book_links:
-        prepared_link = link.replace("https://libgen.is/book/index.php?md5=", "")
-        URL = "http://library.lol/main/" + prepared_link
-        page = requests.get(URL)
-        soup = bs(page.content, 'html.parser')
-        for a in soup.find_all('div', {'id' : 'download'}):
-            for i in a.find_all('h2'):
-                for h in i.find_all('a', href=True):
-                    download_link = (h.get('href'))
-                    download_str_link = str(download_link)
-                    if download_str_link.split(".")[-1] == "pdf": #Without this it also finds sum weird djvu files
-                        download_links.append(download_str_link)
+        download_links = []
+        #open book links
+        for link in all_book_links:
+            prepared_link = link.replace("https://libgen.is/book/index.php?md5=", "")
+            URL = "http://library.lol/main/" + prepared_link
+            page = requests.get(URL, proxies=proxies)
+            soup = bs(page.content, 'html.parser')
+            for a in soup.find_all('div', {'id' : 'download'}):
+                for i in a.find_all('h2'):
+                    for h in i.find_all('a', href=True):
+                        download_link = (h.get('href'))
+                        download_str_link = str(download_link)
+                        if download_str_link.split(".")[-1] == "pdf": #Without this it also finds sum weird djvu files
+                            download_links.append(download_str_link)
 
-    #Let's try to download first link for starters
-    book_index = 1
+        #Let's try to download first link for starters
+        book_index = 1
 
-    #Do sum stuff asynchronously cause me tiref of waiting
-    def convert_book_to_text(pdfpath, txtpath):
-        pdf_text = pdfminer.extract_text(pdfpath)
-        text_file = open(txtpath, "w", encoding='utf8')
-        text_file.writelines(pdf_text)
-        text_file.close()
-        print(f"Assimilated {txtpath.split('/')[-1]}")
+        #Do sum stuff asynchronously cause me tiref of waiting
+        def convert_book_to_text(pdfpath, txtpath):
+            pdf_text = pdfminer.extract_text(pdfpath)
+            text_file = open(txtpath, "w", encoding='utf8')
+            text_file.writelines(pdf_text)
+            text_file.close()
+            print(f"Assimilated {txtpath.split('/')[-1]}")
 
-    for url in download_links:
-        print(f"Downloading file nr. {book_index}")
-        data = requests.get(url, stream=True)
-        datafolder = "/".join([os.getcwd(), suchbegriff.replace(" ", "_")])
-        if not os.path.exists(datafolder):
-            os.makedirs(datafolder)
-        filename = f"book_{book_index}.pdf"
-        textfilename = f"book_{book_index}.txt"
-        full_path = "/".join([datafolder, filename])
-        outputFile = open(full_path, "wb")
-        outputFile.write(data.content)
-        outputFile.close()
-        print("Download complete, starting to parse in background...")
-        
-        full_text_path = "/".join([datafolder, textfilename])
-        parser_thread = Thread(target=convert_book_to_text, args=(full_path, full_text_path))
-        parser_thread.start()   
-        book_index += 1
+        for url in download_links:
+            print(f"Downloading file nr. {book_index}")
+            data = requests.get(url, stream=True, proxies=proxies)
+            datafolder = "/".join([os.getcwd(), suchbegriff.replace(" ", "_")])
+            if not os.path.exists(datafolder):
+                os.makedirs(datafolder)
+            filename = f"book_{book_index}.pdf"
+            textfilename = f"book_{book_index}.txt"
+            full_path = "/".join([datafolder, filename])
+            outputFile = open(full_path, "wb")
+            outputFile.write(data.content)
+            outputFile.close()
+            print("Download complete, starting to parse in background...")
+            
+            full_text_path = "/".join([datafolder, textfilename])
+            parser_thread = Thread(target=convert_book_to_text, args=(full_path, full_text_path))
+            parser_thread.start()   
+            book_index += 1
+    except:
 """
